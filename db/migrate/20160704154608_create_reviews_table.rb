d72b0f09d754491311205f5005cb21c91b5c513a
Nicholas J. Olson <email": null>
2016-07-06 13:53:15 -0500

    Add not null constaints for some tables
    I decided to roll back and edit each table structure.
    For the different migration files. Because it was
    such a small change and would have me just effectively
    re-write everything. Chaning one line across three
    files was the easier and I think better fix.

--- db/migrate/20160704154608_create_reviews_table.rb
+++ db/migrate/20160704154608_create_reviews_table.rb
@@ -1,7 +1,9 @@
 class CreateReviewsTable < ActiveRecord::Migration
   def change
   	create_table :reviews do |t|
+      t.text :review {null: false}
 
+      t.timestamps(null: false)
   	end
   end
 end
