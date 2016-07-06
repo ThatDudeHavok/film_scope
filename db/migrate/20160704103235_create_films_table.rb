0c1846da91f054042d59f2cbbe29d7ada7c05ab5
Nicholas J. Olson <email": null>
2016-07-06 15:47:45 -0500

    Add missing comma to address syntax error

--- db/migrate/20160704103235_create_films_table.rb
+++ db/migrate/20160704103235_create_films_table.rb
@@ -1,7 +1,7 @@
 class CreateFilmsTable < ActiveRecord::Migration
   def change
   	create_table :films do |t|
-			t.string :name {null: false}
+			t.string :name, {null: false}
 			t.string :runtime
 
 			t.timestamp(null: false)
