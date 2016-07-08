138e6a3084f5216d4141cb19b7afb58870eaf007
Nicholas J. Olson <email": null>
2016-07-07 19:43:32 -0500

    Validate review only if filmd_id and reviewer exist

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,15 @@
 class Review < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :user
+  belongs_to :reviewer, {
+    class_name: :User
+  }
   belongs_to :film
+
+  validates :film_id, {
+    presence: true
+  }
+
+  validates :reviewer, {
+    presence: true
+  }
 end
